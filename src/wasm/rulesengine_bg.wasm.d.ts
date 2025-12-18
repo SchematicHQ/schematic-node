82b97c019a961cd79a865f657b4f648d72b2f5b5
Christopher Brady <chris@schematichq.com>
2025-12-18 08:55:29 -0700

    add necessary wasm files

---
+++ src/wasm/rulesengine_bg.wasm.d.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_rulesenginejs_free: (a: number, b: number) => void;
+export const checkFlag: (a: number, b: number, c: number, d: number, e: number, f: number) => number;
+export const evaluateRule: (a: number, b: number, c: number, d: number, e: number, f: number) => number;
+export const getCurrentMetricPeriodStartForCalendarMetricPeriod: (a: number) => bigint;
+export const getCurrentMetricPeriodStartForCompanyBillingSubscription: (a: number, b: number) => bigint;
+export const getNextMetricPeriodStartForCalendarMetricPeriod: (a: number) => bigint;
+export const getNextMetricPeriodStartForCompanyBillingSubscription: (a: number, b: number) => bigint;
+export const getResultJson: () => number;
+export const getResultJsonLength: () => number;
+export const get_version_key_wasm: () => number;
+export const rulesenginejs_checkFlag: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number, number, number];
+export const rulesenginejs_evaluateRule: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number, number];
+export const rulesenginejs_getVersionKey: (a: number) => [number, number];
+export const rulesenginejs_new: () => number;
+export const test_function: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
